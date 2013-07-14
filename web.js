b31a27e92c7bd2be21859ea3086dd9e9a1ffa8ef
Ubuntu <ubuntu@ip-172-31-45-211.us-west-2.compute.internal>
2013-07-14 07:44:33 +0000

    fixed for missing buffer require  modified web.js for index.html input

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var fs = require('fs');
-
+var buffer = require('buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync('index.html')));
+  buf = fs.readFileSync('index.html');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
