286382723c77ac4072a776f1a916d60a1a480106
Ubuntu <ubuntu@ip-172-31-45-211.us-west-2.compute.internal>
2013-07-14 07:30:53 +0000

    fixed for extra parentheses modified web.js for index.html input

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync('index.html'))));
+  response.send(buf.toString(fs.readFileSync('index.html')));
 });
 
 var port = process.env.PORT || 5000;
